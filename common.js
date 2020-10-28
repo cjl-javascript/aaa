c567dc949eb8a12afdbb0aade7ac81f067c886ea
cjl332211 <985251722@qq.com>
2020-10-28 10:58:15 +0800

    1

---
+++ common.js
@@ -0,0 +1,313 @@
+﻿
+
+$(function () {
+    var cityname = $("#msgCityName");
+    var cityId = QueryName("cityId");
+    switch (cityId) {
+        case "51":
+            cityname.text("武汉");
+			$("#telphone").attr("src","/template/All/images/whtel.jpg");
+            break;
+        case "52":
+            cityname.text("长沙");
+			$("#telphone").attr("src","/template/All/images/cstel.jpg");
+            break;
+		case "79":
+			cityname.text("西安");
+			$("#telphone").attr("src","/template/All/images/xiantel.jpg");
+            break;
+    }
+	
+	$(".zuixin ul li span").each(function(){
+		var id=$(this).attr("data-id");
+		if(id==1 ||id==2 || id==3){
+			$(this).addClass("bgyellow").removeClass("bgray");
+		}
+	})
+	$(".wenda ul li span").each(function(){
+		var id=$(this).attr("data-id");
+		if(id==1 ||id==2 || id==3){
+			$(this).addClass("bgyellow").removeClass("bgray");
+		}
+	})
+	$(".baike ul li span").each(function(){
+		var id=$(this).attr("data-id");
+		if(id==1 ||id==2 || id==3){
+			$(this).addClass("bgyellow").removeClass("bgray");
+		}
+	})
+    $(".slist ul").hide();
+	
+    $(".textselect").hover(function () {
+        $("#listxiala").show();
+    },function(){
+		 $("#listxiala").hide();
+	})
+	
+    $("#listxiala li").click(function () {
+        var html = $(this).html();
+		var id=$(this).attr("data-id");
+		$(".textselect").attr("data-id",id)
+        $(".textselect").html(html);
+        $("#listxiala").hide();
+    })
+
+    $("#listxiala").hover(function () {
+		 $(this).show();
+    }, function () {
+        $(this).hide();
+    })
+    $(".dingzhi").hover(function () {
+        $("#dingzhisub").show();
+    },function(){
+		 $("#dingzhisub").hide();
+	})
+	
+    $("#dingzhisub").hover(function () {
+		 $("#dingzhisub").show();
+    }, function () {
+        $(this).hide();
+    })
+
+})
+$(function () {
+    var ID = QueryName("ID");
+    var adname = QueryName("adname");
+    var act = QueryName("act");
+    var url, keybaidu, key360, p;
+    url = window.location.href;
+    keybaidu = QueryName("kw");
+    key360 = QueryName("key360");
+    p = "樱花官网";
+    var CallAddr = "";
+    /*******首页通用注册***********/
+
+    $(".names").blur(function () {
+        if ($(this).val()=="") {
+            layer.tips("请输入姓名！", $(this));
+            return;
+        }
+    })
+    $(".mbs").blur(function () {
+        if ($(this).val().length !=11) {
+            layer.tips("请输入正确的手机号！", $(this));
+            return;
+        }
+    })
+
+    $(".login_btn").click(function () {
+        var names = $(".names").val();
+        var mb = $(".mbs").val();
+        var email = $(".emails").val();
+        var citys = $(".citys").val();
+        var res = verifyCode.validate(document.getElementById("code_input").value);
+        if (names.length == 0) {
+            layer.tips('请输入姓名！',$(".names"));
+            return false;
+        }
+		if (!names.match(/^[\u4E00-\u9FA5a-zA-Z0-9_]{0,}$/)) {
+            layer.tips('姓名不允许有特殊字符！',$(".names"));
+            return false;
+        }
+		if (/^[0-9]*$/.test(names)) {
+		    layer.tips('姓名不允许纯数字！',$(".names"));
+            return false;
+		}
+        if (!mobileOnly(mb)) {
+            layer.tips('请输入正确的手机号！', $(".mbs"));
+            return false;
+        }
+        if (!res){
+            layer.tips('请输入正确的验证码！', $("#code_input"));
+            return false;
+        }
+        // if (email != "") {
+        //     if (!emailOnly(email)) {
+        //         layer.tips('请输入正确的邮箱！', $(".emails"));
+        //         return false;
+        //     }
+        // }
+        //if (citys == "") {
+            //layer.tips('请选择你所在的城市！', $(".citys"));
+            //return false;
+        //}
+        AjaxRegister(names, mb, email, citys, ID, adname, act, '', keybaidu, key360, '樱花');
+    })
+    /**********申请免费日语课程注册**********/
+    $("#btnSubmit1").click(function () {
+        var CallAddr = "";
+        var names = $(".names").val();
+        var mb = $(".mbs").val();
+        var email = $(".emails").val();
+        var citys = $(".citys").find("option:selected").text();
+        if (names == "") {
+            layer.tips('请输入姓名！', $(".names"));
+            return false;
+        }
+		if (!names.match(/^[\u4E00-\u9FA5a-zA-Z0-9_]{0,}$/)) {
+            layer.tips('姓名不允许有特殊字符！',$(".names"));
+            return false;
+        }
+		if (/^[0-9]*$/.test(names)) {
+		    layer.tips('姓名不允许纯数字！',$(".names"));
+            return false;
+		}
+        if (!mobileOnly(mb)) {
+            layer.tips('请输入正确的手机号！', $(".mbs"));
+            return false;
+        }
+        if (email != "") {
+            if (!emailOnly(email)) {
+                layer.tips('请输入正确的邮箱！', $(".emails"));
+                return false;
+            }
+        }
+        //if (citys == "选择城市") {
+            //layer.msg('请选择你所在的城市！');
+            //return false;
+        //}
+        AjaxRegister(names, mb, email, citys, ID, adname, act, '', keybaidu, key360, '樱花');
+    })
+	
+	 $("#btnSubmit2").click(function () {
+        var CallAddr = "";
+        var names = $("#names").val();
+        var mb = $("#mbs").val();
+        var email = $("#emails").val();
+        var citys = $("#citys").val();
+        if (names == "") {
+            layer.tips('请输入姓名！', $("#names"));
+            return false;
+        }
+		if (!names.match(/^[\u4E00-\u9FA5a-zA-Z0-9_]{0,}$/)) {
+            layer.tips('姓名不允许有特殊字符！',$("#names"));
+            return false;
+        }
+		if (/^[0-9]*$/.test(names)) {
+		    layer.tips('姓名不允许纯数字！',$("#names"));
+            return false;
+		}
+        if (!mobileOnly(mb)) {
+            layer.tips('请输入正确的手机号！', $("#mbs"));
+            return false;
+        }
+        if (email != "") {
+            if (!emailOnly(email)) {
+                layer.tips('请输入正确的邮箱！', $("#emails"));
+                return false;
+            }
+        }
+        //if (citys == "") {
+            //layer.msg('请选择你所在的城市！');
+            //return false;
+        //}
+        AjaxRegister(names, mb, email, citys, ID, adname, act, '', keybaidu, key360, '樱花');
+    })
+    
+})
+
+function AjaxRegister(a, b, c, d, e, f, g, h, k, l, m) {
+    $.ajax({
+        url: "/Api/actionResult.ashx?action=register",
+        dataType: "text",
+        type: "post",
+        data: { "username": a, "mobile": b, "email": c, "seladdr": d, "ID": e, "adname": f, "act": g, "age": 0, "kw": k, "key360": l, "P": m, "url": encodeURIComponent(window.location.href) },
+        success: function (data) {
+            if (data == "1") {
+                layer.msg("恭喜您，注册成为会员！", { icon: 1 }, function () {
+                    parent.location.href = '/free/tiaozhuan.html';
+                })
+            } else if (data == "2") {
+                layer.msg('对不起,该手机号已存在!', { icon: 8 });
+            } else {
+                layer.msg("注册失败!", { icon: 8 })
+            }
+        }
+    })
+}
+
+function loading() {
+    layer.load();
+}
+function getRootPath() {
+    var curWwwPath = window.document.location.href;
+    var pathName = window.document.location.pathname;
+    var pos = curWwwPath.indexOf(pathName);
+    var localhostPaht = curWwwPath.substring(0, pos);
+    var projectName = pathName.substring(0, pathName.substr(1).indexOf('/') + 1);
+    return (localhostPaht + projectName);
+}
+
+function QueryName(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+var mobileOnly = function (str) {
+    var regMobile = /^0{0,1}(13[0-9]|14[0-9]|15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-9])[0-9]{8}$/;
+    if (regMobile.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//纯数字验证
+var numOnly = function (str) {
+    var regNum = /^[0-9]{1,20}$/;
+    if (regNum.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//电话号码验证
+var telOnly = function (str) {
+    var regTel = /^(\d{3,4}-)?\d{7,9}$/g;
+    if (regTel.test(str))
+        return true;
+    else
+        return false;
+}
+//电子邮件验证
+var emailOnly = function (str) {
+    var regEmail = /[_a-zA-Z\d\-\.]+@[_a-zA-Z\d\-]+(\.[_a-zA-Z\d\-]+)+$/;
+    if (regEmail.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//弹出层
+function Diloag(t, w, h, s) {
+    layer.open({
+        type: 2,
+        title: t,
+        area: [w + "px", h + "px"],
+        fix: true, //不固定
+        maxmin: false,
+        content: s
+    });
+}
+
+//百度统计代码---语培 
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9afe53ca909bd0bba8e614e9f824bc1f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+// 验证码
+$(document).ready(function(){
+    $("head").append('<script src="/templates/main/js/yzm.js"></script>');
+    if($('#login_btn').length > 0){
+        $('body').append('<script>var verifyCode = new GVerify("v_container");</script>')    
+    }   
+})
+  
+
+
+
---
+++ xsjrblx/script/xsjcommon.js
@@ -0,0 +1,465 @@
+﻿var ID = "";
+var adname = "";
+var act = "";
+var mag = "";
+var kw = "";
+var keybaidu = "";
+var key360 = "";
+var cutParms = "";
+var msg = "";
+//验证码默认关闭
+var yzm = "f";
+//倒计时--验证码
+var wait = 60;
+//按钮等待
+var btnwait = 2;
+//注册类型 默认日语
+var pType = 2;
+//乐语的开启关闭
+var ly="";
+
+
+$(function () {
+    ID = GetParms("ID");
+    adname = GetParms("adname");
+    act = GetParms("act");
+    kw = GetParms("kw");
+    keybaidu = GetParms("keybaidu");
+    key360 = GetParms("key360");
+    cutParms = CutParms();
+
+
+    //显示验证码
+    yzm = GetParms("vc");
+    if (yzm != "t") {
+        $(".yzm").css("display", "none");
+    }
+    if (yzm == "t") {
+        $('.yzm').hide();
+
+        $('.ipt-p').focus(function () {
+            $('.yzm').show();
+        })
+        $(".ipt-p").blur(function () {
+            if ($(this).val().length < 1) {
+                $('.yzm').hide();
+            }
+        })
+
+    }
+    //有乐语
+    ly = GetParms("cityid")
+    if (ly == 'quanguo') {
+        $("head").append('<script type="text/javascript" charset="utf-8" src="https://op.jiain.net/20000967/10108920.js"></script>');
+        $('.zixun,.zixun2,.img4').click(function () {
+            doyoo.util.openChat('g=10084377');
+        })
+    }
+    //无乐语
+    if (ly == 'quangu') {
+
+        $('.zixun,.zixun2,.img4').click(function () {
+            window.scrollTo(0, document.documentElement.scrollHeight - document.documentElement.clientHeight);
+        })
+    }
+    //获取验证码
+    $("#btncode").click(function () {
+        var mobile = $.trim($("#Orders_Tel").val());
+        if (mobileOnly(mobile) == false) {
+            alert('请输入正确的手机号!');
+            return;
+        }
+        time(this);
+        $.ajax({
+            url: "https://www.sakurajp.com.cn/api/XsjSendMsg.ashx?mobile=" + mobile,
+            type: "get",
+            dataType: "text",
+            success: function (data) {
+                if (data == "") {
+                    alert('验证码发送失败!');
+                } else {
+
+                    setCookie("salt", data, 1, '', '', '');
+                }
+            }
+        })
+
+    })
+
+    $("#btncode1").click(function () {
+        var mobile = $.trim($("#Orders_Tel2").val());
+        if (mobileOnly(mobile) == false) {
+            alert('请输入正确的手机号!');
+            return;
+        }
+        time(this);
+        $.ajax({
+            url: "https://www.sakurajp.com.cn/api/XsjSendMsg.ashx?mobile=" + mobile,
+            type: "get",
+            dataType: "text",
+            success: function (data) {
+                if (data == "") {
+                    alert('验证码发送失败!');
+                } else {
+
+                    setCookie("salt", data, 1, '', '', '');
+                }
+            }
+        })
+
+    })
+
+
+
+    $("#btnsubmit").click(function () {
+        btnwaittime(this);
+        return Register("Orders_Name", "Orders_Tel");
+    })
+    $("#btnsubmit4").click(function () {
+        btnwaittime(this);
+        return RegisterLX("Orders_Name", "Orders_Tel");
+    })
+    $("#btnsubmit5").click(function () {
+        btnwaittime(this);
+        return RegisterLX2("Orders_Name2", "Orders_Tel2");
+    })
+    $("#btnsubmit3").click(function () {
+        btnwaittime(this);
+        return Register("Orders_Name3", "Orders_Tel3");
+    })
+
+    $("#btnsubmit2").click(function () {
+        btnwaittime(this);
+        return Register("Orders_Name2", "Orders_Tel2");
+    })
+    $("#btnsubmit1").click(function () {
+        btnwaittime(this);
+        return Register("Orders_Name1", "Orders_Tel1");
+    })
+
+    $("#btnsubmitYZM").click(function () {
+        btnwaittime(this);
+        if (yzm == "t") {
+            var code = $.trim($("#YZMcode").val());
+            var codeVal = getCookie("salt");
+            if (code == "") {
+                alert('请输入验证码！');
+                return false;
+            }
+            if (code != codeVal) {
+                alert('您输入的验证码不正确！');
+                return false;
+            }
+        }
+        return Register("Orders_Name", "Orders_Tel");
+    })
+
+
+    $("#btnsubmitYZM1").click(function () {
+        btnwaittime(this);
+        if (yzm == "t") {
+            var code = $.trim($("#YZMcode1").val());
+            var codeVal = getCookie("salt");
+            if (code == "") {
+                alert('请输入验证码！');
+                return false;
+            }
+            if (code != codeVal) {
+                alert('您输入的验证码不正确！');
+                return false;
+            }
+        }
+
+
+        return Register("Orders_Name1", "Orders_Tel1");
+    })
+    //英语注册
+    $("#btnsubmit_PEP").click(function () {
+        btnwaittime(this);
+        pType = 3;
+        return Register("Orders_Name", "Orders_Tel");
+    })
+
+})
+
+function time(o) {
+    if (wait == 0) {
+        o.removeAttribute("disabled");
+        o.value = "获取验证码";
+        wait = 60;
+    } else {
+        o.setAttribute("disabled", "disabled");
+        o.value = wait + "s后可重发";
+        wait--;
+        setTimeout(function () {
+            time(o)
+        },
+            1000)
+    }
+}
+
+//按钮等待 3秒
+function btnwaittime(o) {
+    if (btnwait == 0) {
+        o.removeAttribute("disabled");
+        btnwait = 2;
+    } else {
+        o.setAttribute("disabled", "disabled");
+        btnwait--;
+        setTimeout(function () {
+            btnwaittime(o)
+        },
+            1000)
+    }
+}
+
+//普通注册
+function Register(nameID, mobileID) {
+    var dname = $("#" + nameID);
+    var dmobile = $("#" + mobileID);
+    var username = $.trim(dname.val());
+    var mobile = $.trim(dmobile.val());
+    if (username == "") {
+        alert('请输入姓名！');
+        return false;
+    }
+    if (username.length >= 8) {
+        alert('姓名长度超过8位！');
+        return false;
+    }
+    if (!username.match(/^[\u4E00-\u9FA5a-zA-Z0-9_]{0,}$/)) {
+        alert('姓名不允许有特殊字符！');
+        return false;
+    }
+    if (/^[0-9]*$/.test(username)) {
+        alert('姓名不允许纯数字！');
+        return false;
+    }
+    if (mobile.length != 11) {
+        alert('手机号必须是11位！');
+        return false;
+    }
+    if (mobileOnly(mobile) == false) {
+        alert('请输入正确的手机号！');
+        return false;
+    }
+
+    if (yzm == "t") {
+        var code = $.trim($("#YZMcode").val());
+        var codeVal = getCookie("salt");
+        if (code == "") {
+            alert('请输入验证码！');
+            return false;
+        }
+        if (code != codeVal) {
+            alert('您输入的验证码不正确！');
+            return false;
+        }
+    }
+
+    AjaxRegister(username, mobile, '', '', ID, adname, act, '', kw, key360, pType);
+
+    return true;
+}
+//留学注册
+function RegisterLX(nameID, mobileID) {
+    var dname = $("#" + nameID);
+    var dmobile = $("#" + mobileID);
+    var username = $.trim(dname.val());
+    var mobile = $.trim(dmobile.val());
+    if (username == "") {
+        alert('请输入姓名！');
+        return false;
+    }
+    if (username.length >= 8) {
+        alert('姓名长度超过8位！');
+        return false;
+    }
+    if (!username.match(/^[\u4E00-\u9FA5a-zA-Z0-9_]{0,}$/)) {
+        alert('姓名不允许有特殊字符！');
+        return false;
+    }
+    if (/^[0-9]*$/.test(username)) {
+        alert('姓名不允许纯数字！');
+        return false;
+    }
+    if (mobile.length != 11) {
+        alert('手机号必须是11位！');
+        return false;
+    }
+    if (mobileOnly(mobile) == false) {
+        alert('请输入正确的手机号！');
+        return false;
+    }
+
+    if (yzm == "t") {
+        var code = $.trim($("#YZMcode").val());
+        var codeVal = getCookie("salt");
+        if (code == "") {
+            alert('请输入验证码！');
+            return false;
+        }
+        if (code != codeVal) {
+            alert('您输入的验证码不正确！');
+            return false;
+        }
+    }
+
+    AjaxRegister(username, mobile, '', '', ID, adname, act, '', kw, key360, 1);
+
+    return true;
+}
+//留学注册2
+function RegisterLX2(nameID, mobileID) {
+    var dname = $("#" + nameID);
+    var dmobile = $("#" + mobileID);
+    var username = $.trim(dname.val());
+    var mobile = $.trim(dmobile.val());
+    if (username == "") {
+        alert('请输入姓名！');
+        return false;
+    }
+    if (username.length >= 8) {
+        alert('姓名长度超过8位！');
+        return false;
+    }
+    if (!username.match(/^[\u4E00-\u9FA5a-zA-Z0-9_]{0,}$/)) {
+        alert('姓名不允许有特殊字符！');
+        return false;
+    }
+    if (/^[0-9]*$/.test(username)) {
+        alert('姓名不允许纯数字！');
+        return false;
+    }
+    if (mobile.length != 11) {
+        alert('手机号必须是11位！');
+        return false;
+    }
+    if (mobileOnly(mobile) == false) {
+        alert('请输入正确的手机号！');
+        return false;
+    }
+
+    if (yzm == "t") {
+        var code = $.trim($("#YZMcode2").val());
+        var codeVal = getCookie("salt");
+        if (code == "") {
+            alert('请输入验证码！');
+            return false;
+        }
+        if (code != codeVal) {
+            alert('您输入的验证码不正确！');
+            return false;
+        }
+    }
+
+    AjaxRegister(username, mobile, '', '', ID, adname, act, '', kw, key360, 1);
+
+    return true;
+}
+//注册方法
+function AjaxRegister(name, mobile, email, seladdr, ID, adname, act, calladdr, k, key360, m) {
+
+    $.ajax({
+        url: "https://www.sakurajp.com.cn/Api/xsjResult.ashx?action=register",
+        dataType: "text",
+        type: "post",
+        data: { "username": name, "mobile": mobile, "email": email, "seladdr": seladdr, "ID": ID, "adname": adname, "act": act, "calladdr": calladdr, "age": 0, "keybaidu": k, "key360": key360, "P": m, "url": window.parent.location.href },
+        beforeSend: function () {
+        },
+        success: function (data) {
+            if (data == "1") {
+                alert("注册成功");
+            } else if (data == "2") {
+                alert("对不起,该手机号已存在!");
+            } else {
+                alert("注册失败");
+            }
+        }
+    })
+}
+
+//获取参数
+function GetParms(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]); return null;
+}
+//截取参数
+
+function CutParms() {
+    var reg2 = /([^?]+)$/;
+    var r = window.location.search.substr(1).match(reg2);
+    if (r != null) return r[1]; return null;
+}
+
+// 手机号码验证
+var mobileOnly = function (str) {
+    var regMobile = /^0{0,1}(13[0-9]|14[0-9]|15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-9])[0-9]{8}$/;
+    if (regMobile.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+//纯数字验证
+var numOnly = function (str) {
+    var regNum = /^[0-9]{1,20}$/;
+    if (regNum.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//电话号码验证
+var telOnly = function (str) {
+    var regTel = /^(\d{3,4}-)?\d{7,9}$/g;
+    if (regTel.test(str))
+        return true;
+    else
+        return false;
+}
+//电子邮件验证
+var emailOnly = function (str) {
+    var regEmail = /[_a-zA-Z\d\-\.]+@[_a-zA-Z\d\-]+(\.[_a-zA-Z\d\-]+)+$/;
+    if (regEmail.test(str)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function getCookie(name) {
+    var start = document.cookie.indexOf(name + "=");
+    var len = start + name.length + 1;
+    if ((!start) && (name != document.cookie.substring(0, name.length))) {
+        return null;
+    }
+    if (start == -1) return null;
+    var end = document.cookie.indexOf(';', len);
+    if (end == -1) end = document.cookie.length;
+    return unescape(document.cookie.substring(len, end));
+}
+
+function setCookie(name, value, expires, path, domain, secure) {
+    var today = new Date();
+    today.setTime(today.getTime());
+    if (expires) {
+        expires = expires * 1000 * 60 * 60 * 24;
+    }
+    var expires_date = new Date(today.getTime() + (expires));
+    document.cookie = name + '=' + escape(value) +
+        ((expires) ? ';expires=' + expires_date.toGMTString() : '') + //expires.toGMTString()  
+        ((path) ? ';path=' + path : '') +
+        ((domain) ? ';domain=' + domain : '') +
+        ((secure) ? ';secure' : '');
+}
+
+
+
+
+
+
+
